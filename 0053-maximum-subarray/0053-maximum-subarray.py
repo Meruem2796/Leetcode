95418eb571fd55c8ff88a4049163a7c0f2b45834
Meruem2796 <126846140+Meruem2796@users.noreply.github.com>
2024-04-12 04:56:47 +0200

    Delete 0053-maximum-subarray directory

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++
@@ -1,8 +0,0 @@
-class Solution:
-    def maxSubArray(self, nums: List[int]) -> int:
-        max_sum = float('-inf')  # Initialiser à une valeur négative infinie
-        current_sum = 0  # Somme courante du sous-tableau
-        for num in nums:
-            current_sum = max(num, current_sum + num)
-            max_sum = max(max_sum, current_sum)
-        return max_sum
