2d9522702ec03edc373370f5c14ebe788c874a41
Meruem2796 <126846140+Meruem2796@users.noreply.github.com>
2024-04-12 05:02:20 +0200

    Delete 1683-maximum-number-of-coins-you-can-get directory

--- 1683-maximum-number-of-coins-you-can-get/1683-maximum-number-of-coins-you-can-get.py
+++
@@ -1,9 +0,0 @@
-class Solution(object):
-    def maxCoins(self, piles):
-        """
-        :type piles: List[int]
-        :rtype: int
-        """
-        piles.sort()
-        n = len(piles) // 3
-        return sum(piles[n::2])
