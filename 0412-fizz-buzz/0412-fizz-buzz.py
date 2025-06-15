5f6a887a27c3e1e83a1bd2496fad3d3868ec6474
Abdou Mourid AGBO-OLA <126846140+Meruem2796@users.noreply.github.com>
2025-06-15 09:30:04 +0200

    Time: 0 ms (100%), Space: 18.4 MB (43.36%) - LeetHub

--- 0412-fizz-buzz/0412-fizz-buzz.py
+++ 0412-fizz-buzz/0412-fizz-buzz.py
@@ -2,7 +2,7 @@ class Solution:
     def fizzBuzz(self, n: int) -> List[str]:
         answer = []
         for i in range(1, n + 1):
-            if i % 15 == 0:
+            if i % 3 == 0 and i % 5 == 0:
                 answer.append("FizzBuzz")
             elif i % 5 == 0:
                 answer.append("Buzz")
