2832ac9f18b6eec1a545f845dd507bcde8ac6734
Abdou Mourid AGBO-OLA <126846140+Meruem2796@users.noreply.github.com>
2025-06-15 19:40:49 +0200

    Time: 7 ms (69.03%), Space: 18 MB (7.73%) - LeetHub

--- 0728-self-dividing-numbers/0728-self-dividing-numbers.py
+++ 0728-self-dividing-numbers/0728-self-dividing-numbers.py
@@ -1,17 +1,30 @@
-class Solution(object):
-    def selfDividingNumbers(self, left, right):
-        """
-        :type left: int
-        :type right: int
-        :rtype: List[int]
-        """
-        
-        self_dividing_nums = []
+from typing import List
+
+
+class Solution:
+    def selfDividingNumbers(self, left: int, right: int) -> List[int]:
+        result = []
 
         for num in range(left, right + 1):
-            if "0" in str(num):
-                continue
-            if all(num % digit == 0 for digit in map(int, str(num))):
-                self_dividing_nums.append(num)
+            if self.is_self_dividing(num):
+                result.append(num)
 
-        return self_dividing_nums
+        return result
+
+    def is_self_dividing(self, num: int) -> bool:
+        """Vérifie si un nombre est auto-divisible"""
+        # Les nombres à un chiffre (1-9) sont auto-divisibles par définition
+        if num < 10:
+            return True
+
+        # Si le nombre contient 0, il ne peut pas être auto-divisible
+        if "0" in str(num):
+            return False
+
+        # Vérifier que tous les chiffres divisent le nombre
+        for digit_char in str(num):
+            digit = int(digit_char)
+            if num % digit != 0:
+                return False
+
+        return True
