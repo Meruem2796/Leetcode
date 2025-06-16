85249d902da3f88f3804f7747cd50d8a3d0c639d
Abdou Mourid AGBO-OLA <126846140+Meruem2796@users.noreply.github.com>
2025-06-16 08:05:27 +0200

    Time: 14 ms (4.57%), Space: 13.3 MB (9.67%) - LeetHub

---
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def numIdenticalPairs(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        return sum(1 for i in range(len(nums)) for j in range(i + 1, len(nums)) if nums[i] == nums[j])
