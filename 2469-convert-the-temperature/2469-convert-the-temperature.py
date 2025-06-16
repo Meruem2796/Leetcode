ddc7f8024906abac1b13a8ac028061adbfc0b21a
Abdou Mourid AGBO-OLA <126846140+Meruem2796@users.noreply.github.com>
2025-06-16 21:53:03 +0200

    Time: 0 ms (100%), Space: 18 MB (17.22%) - LeetHub

--- 2469-convert-the-temperature/2469-convert-the-temperature.py
+++ 2469-convert-the-temperature/2469-convert-the-temperature.py
@@ -1,10 +1,6 @@
-class Solution(object):
-    def convertTemperature(self, celsius):
-        """
-        :type celsius: float
-        :rtype: List[float]
-        """
-        kelvin = round(celsius + 273.15, 5)
-        fahrenheit = round(celsius * 1.80 + 32.00, 5) 
-        ans = [kelvin, fahrenheit]
+class Solution:
+    def convertTemperature(self, celsius: float) -> List[float]:
+        Kelvin = round(celsius + 273.15, 5)
+        Fahrenheit = round(celsius * 1.8 + 32.00, 5)
+        ans = [Kelvin, Fahrenheit]
         return ans
