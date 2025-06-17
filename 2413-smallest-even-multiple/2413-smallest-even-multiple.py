cbef5529bcb6909fd300a6baf095507ff2e3a592
Abdou Mourid AGBO-OLA <126846140+Meruem2796@users.noreply.github.com>
2025-06-17 14:14:13 +0200

    Time: 0 ms (100%), Space: 17.7 MB (87.83%) - LeetHub

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.py
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.py
@@ -1,18 +1,8 @@
-class Solution(object):
-    def smallestEvenMultiple(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        multiple = 0
-        
-        if n == 1:
-            return 2*n
+class Solution:
+    def smallestEvenMultiple(self, n: int) -> int:
+        if n % 2 != 0:
+            output = 2 * n
+        else:
+            output = n
 
-        for i in range(2, n+1):
-            if i % 2 == 0 and i % n == 0:
-                multiple = i
-            else:
-                multiple = 2*n
-                
-        return multiple
+        return output
