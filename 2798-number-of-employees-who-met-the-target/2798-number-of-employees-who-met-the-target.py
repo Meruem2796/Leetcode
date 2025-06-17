a842e9ae95f8924acea09a7e95b932caafa3edd6
Abdou Mourid AGBO-OLA <126846140+Meruem2796@users.noreply.github.com>
2025-06-17 13:53:23 +0200

    Time: 0 ms (100%), Space: 17.7 MB (51.3%) - LeetHub

--- 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.py
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.py
@@ -1,9 +1,7 @@
-class Solution(object):
-    def numberOfEmployeesWhoMetTarget(self, hours, target):
-        """
-        :type hours: List[int]
-        :type target: int
-        :rtype: int
-        """
-        number = sum(1 for i in hours if i >= target)
-        return number
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        output = 0
+        for h in hours:
+            if h >= target:
+                output += 1
+        return output
