29ddc81f55bacaf32e43774ddaf8f6bd88d727e7
Abdou Mourid AGBO-OLA <126846140+Meruem2796@users.noreply.github.com>
2025-06-17 13:52:56 +0200

    Time: 30 ms (1.33%), Space: 13.2 MB (9.31%) - LeetHub

---
+++ 2798-number-of-employees-who-met-the-target/2798-number-of-employees-who-met-the-target.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def numberOfEmployeesWhoMetTarget(self, hours, target):
+        """
+        :type hours: List[int]
+        :type target: int
+        :rtype: int
+        """
+        number = sum(1 for i in hours if i >= target)
+        return number
