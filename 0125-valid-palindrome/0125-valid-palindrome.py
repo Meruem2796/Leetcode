38c9118fa8a75dabbbbf9f207086e776624097a4
Abdou Mourid AGBO-OLA <126846140+Meruem2796@users.noreply.github.com>
2025-06-18 12:12:14 +0200

    Time: 25 ms (33.89%), Space: 14.5 MB (13.78%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        alphanumeric_chars = [char.lower() for char in s if char.isalnum()]
+
+        return alphanumeric_chars == alphanumeric_chars[::-1]
